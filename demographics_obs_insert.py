ba0739198b458fe3ee7eb61296edbc1df7a33575
Henrygicugu <hnjuguna@healthstrat.co.ke>
2025-06-20 07:14:15 +0300

    saddjhkjj

--- demographics_obs_insert.py
+++ demographics_obs_insert.py
@@ -142,7 +142,7 @@ def main():
             continue
 
         for field, config in concept_map.items():
-            value = cast_to_number(row.get(field))
+            value = row.get(field)
             if config["type"] == "coded":
                 if field == "implementing_partner_id":
                     value = ip_map.get(str(value))
