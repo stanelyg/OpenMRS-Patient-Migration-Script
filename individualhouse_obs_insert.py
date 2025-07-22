de4a3fd12f31c3c250351d55675e88ffdedbf2a0
stanelyg <henrygicugu@gmail.com>
2025-07-22 03:45:30 +0300

    shgxhgHZGhghl

--- individualhouse_obs_insert.py
+++ individualhouse_obs_insert.py
@@ -125,7 +125,7 @@ def main():
         )
         AND EXISTS (
             SELECT 1 FROM tbl_m_demographics d 
-            WHERE d.client_id = h.client_id AND d.implementing_partner_id IN  IN (12,35)
+            WHERE d.client_id = h.client_id AND d.implementing_partner_id IN (12,35)
         )
     """)
     for row in cursor.fetchall():
