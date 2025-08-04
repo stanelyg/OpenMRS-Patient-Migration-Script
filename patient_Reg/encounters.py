dc70f644605905034ba5d6e6edbae9e9908834c5
henry <henry@Ubuntu.myguest.virtualbox.org>
2025-08-05 01:12:54 +0300

    sasayuyk

--- patient_Reg/encounters.py
+++ patient_Reg/encounters.py
@@ -78,7 +78,7 @@ def insert_encounters():
 
         # Log into dreams_production.`service_uptake_encounter_mapping`
         cursor.execute("""
-            INSERT INTO social_protection_encounter_mapping (patient_id, encounter_id)
+            INSERT INTO other_intervention_encounter_mapping (patient_id, encounter_id)
             VALUES (%s, %s)
         """, (patient_id, encounter_id))
 
