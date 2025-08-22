5162f0a3211724c7b923289c496753d2dffa7325
stanelyg <henrygicugu@gmail.com>
2025-08-22 17:36:37 +0300

    ashgdghgh

--- patient_Reg/main.py
+++ patient_Reg/main.py
@@ -7,13 +7,6 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-# Source DB (client data)
-SOURCE_DB = {
-    'host': os.getenv('SOURCE_DB_HOST'),
-    'user': os.getenv('SOURCE_DB_USER'),
-    'password': os.getenv('SOURCE_DB_PASSWORD'),
-    'database': os.getenv('SOURCE_DB_NAME')
-}
 
 # Destination DB (OpenMRS)
 DEST_DB = {
@@ -29,20 +22,22 @@ LOCATION_ID = 1
 
 
 def migrate_patients():
-    source_conn = mysql.connector.connect(**SOURCE_DB)
-    dest_conn = mysql.connector.connect(**DEST_DB)
+    conn = mysql.connector.connect(**DEST_DB)
+    cursor = conn.cursor(dictionary=True)
 
-    source_cursor = source_conn.cursor(dictionary=True)
-    dest_cursor = dest_conn.cursor()
-
-    source_cursor.execute("SELECT * FROM client_flat where client_id =2") 
-    patients = source_cursor.fetchall()
+    cursor.execute("""SELECT * FROM tbl_client_flat_vuci a
+                            WHERE NOT EXISTS (
+                                SELECT 1
+                                FROM dreams_client_patient_mapping b
+                                WHERE b.client_id = a.client_id
+                            )""") 
+    patients = cursor.fetchall()
     for row in patients:
         now = datetime.now()
         person_uuid = str(uuid.uuid4())
 
         # Insert into person
-        dest_cursor.execute("""
+        cursor.execute("""
             INSERT INTO person (gender, birthdate, birthdate_estimated, dead, creator, date_created, uuid)
             VALUES (%s, %s, %s, %s, %s, %s, %s)
         """, (
@@ -55,10 +50,10 @@ def migrate_patients():
             person_uuid
         ))
 
-        person_id = dest_cursor.lastrowid
+        person_id = cursor.lastrowid
 
         # Insert into person_name
-        dest_cursor.execute("""
+        cursor.execute("""
             INSERT INTO person_name (person_id, given_name, middle_name, family_name, preferred, creator, date_created, uuid)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """, (
@@ -73,7 +68,7 @@ def migrate_patients():
         ))
 
         # Insert into person_address
-        dest_cursor.execute("""
+        cursor.execute("""
             INSERT INTO person_address (person_id, address1, address2, city_village, county_district, state_province, address4, uuid, creator, date_created)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (
@@ -98,13 +93,13 @@ def migrate_patients():
 
         for attr_type_id, value in attributes.items():
             if value:
-                dest_cursor.execute("""
+                cursor.execute("""
                     INSERT INTO person_attribute (person_id, value, person_attribute_type_id, creator, date_created, uuid)
                     VALUES (%s, %s, %s, %s, %s, %s)
                 """, (person_id, value, attr_type_id, 1, now, str(uuid.uuid4())))
         
         # patient
-        dest_cursor.execute("""
+        cursor.execute("""
             INSERT INTO patient (patient_id, creator, date_created)
             VALUES (%s, %s, %s)
         """, (person_id, CREATOR_ID, now))
@@ -119,7 +114,7 @@ def migrate_patients():
         for identifier_type_id, value in identifiers.items():
             if value:
                 trimmed_identifier = value.strip()[:50]
-                dest_cursor.execute("""
+                cursor.execute("""
                     INSERT INTO patient_identifier
                     (patient_id, identifier, identifier_type, location_id, preferred, creator, date_created, uuid)
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
@@ -130,14 +125,14 @@ def migrate_patients():
         # Fallback: Generate OpenMRS-style identifier if none was available
         if not identifier_inserted:
             fallback_id = f"PT{person_id:06}" # Or use person_id-based
-            dest_cursor.execute("""
+            cursor.execute("""
                 INSERT INTO patient_identifier
                 (patient_id, identifier, identifier_type, location_id, preferred, creator, date_created, uuid)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             """, (person_id, fallback_id, 1, 1, 1, 1, now, str(uuid.uuid4())))
         
         # map client to patient
-        dest_cursor.execute("""
+        cursor.execute("""
                    INSERT INTO  dreams_client_patient_mapping (client_id,patient_id)
                    VALUES (%s,%s)""",(row.get('client_id'),person_id))
 
@@ -145,11 +140,8 @@ def migrate_patients():
 
         print(f"Migrated:(person_id {person_id})")
 
-    dest_conn.commit()
-    source_cursor.close()
-    dest_cursor.close()
-    source_conn.close()
-    dest_conn.close()
+    conn.commit()
+    cursor.close()
 
 if __name__ == "__main__":
     migrate_patients()
